0c706804c859de3e1aa07f0bba0d2edd8fd7e1c8
mariopdls <marioporra9@gmail.com>
2026-06-30 04:37:21 +0200

    fix logo3 import casing in App

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import Perfil from './pages/Perfil'
 import Amigos from './pages/Amigos'
 import Cargando from './components/Cargando'
 import ModalConfig from './components/ModalConfig'
-import logo from './assets/logo3.PNG'
+import logo from './assets/logo3.png'
 import lohago from './assets/lohago.PNG'
 import i18n from './i18n/i18n.js'
 
